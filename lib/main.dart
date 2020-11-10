d12837d2149fcb1b8c4fdcc796542e1e28aee1e3
Yasee Agwan <yaseen04071998@gmail.com>
2020-11-10 22:08:33 +0530

    added firebae config

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,24 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'todo_app.dart';
 import 'task_form.dart';
-void main() => runApp(MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-      
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        routes: {
-            '/AddTask': (context) => AddTask(),
-        },
-        home: TodoApp(),
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/AddTask': (context) => AddTask(),
+      },
+      home: TodoApp(),
     );
   }
 }
-
--- lib/task_form.dart
+++ lib/task_form.dart
@@ -1,69 +1,72 @@
+// import ‘package:cloud_firestore/cloud_firestore.dart’;
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'tasks.dart';
-class AddTask extends StatelessWidget{
-    final formKey = GlobalKey<FormState>();
-    final taskTitle = TextEditingController();
-    final task = TextEditingController();
-    //Remove Scaffold once Integrated with Routes
-    @override
-    Widget build(BuildContext context){
-        return Scaffold(
-            appBar: AppBar(
-                title: Text('Add Task'),
-            ),
-            body: Form(
-                key: formKey,
-                child: Column(
-                    children:[
-                        SizedBox(height: 10.0),
-                        TextFormField(
-                            controller: taskTitle,
-                            decoration: const InputDecoration(
-                              labelText:'Task Title',
-                              hintText: 'xyz Work'  
-                            ),
-                            validator: (value){
-                                if(value.isEmpty){
-                                    return 'Title Cannot be Empty';
-                                }
-                                return null;
-                            }
-                        ),
-                        SizedBox(height: 15.0),
-                        Container(
-                            decoration: BoxDecoration(
-                                border: Border.all(
-                                    color: Colors.blue[100],
-                                ),
-                            ),
-                            child: TextFormField(
-                                controller: task,
-                                maxLines: 20,
-                                decoration: const InputDecoration(
-                                    hintText: 'Write your task here',
-                                ),
-                            ),
-                        ),
-                        SizedBox(height: 15.0),
-                        RaisedButton(
-                            color: Theme.of(context).primaryColor,
-                            child: Text('Add Task'),
-                            onPressed:(){
-                                    // var tasksObj = new Task(taskTitle.value.toString(),task.value.toString());
-                                    // var tasks1 = Tasks();
-                                    // tasks1.addTask(tasksObj);
-                                    formKey.currentState.validate();
-                                    print('Title : ${taskTitle.value}');
-                                    print('Task : ${task.value}');
-                                    Scaffold.of(context)
-                                        .showSnackBar(SnackBar(content: Text('Processing Data')));
-                            
-                            }
-                        )
-                    ]
-                ),
-            ),
-        );
-    }
-}
 
+class AddTask extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
+  final taskTitle = TextEditingController();
+  final task = TextEditingController();
+  //Remove Scaffold once Integrated with Routes
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add Task'),
+      ),
+      body: Form(
+        key: formKey,
+        child: Column(children: [
+          SizedBox(height: 10.0),
+          TextFormField(
+              controller: taskTitle,
+              decoration: const InputDecoration(
+                  labelText: 'Task Title', hintText: 'xyz Work'),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Title Cannot be Empty';
+                }
+                return null;
+              }),
+          SizedBox(height: 15.0),
+          Container(
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.blue[100],
+              ),
+            ),
+            child: TextFormField(
+              controller: task,
+              maxLines: 20,
+              decoration: const InputDecoration(
+                hintText: 'Write your task here',
+              ),
+            ),
+          ),
+          SizedBox(height: 15.0),
+          RaisedButton(
+              color: Theme.of(context).primaryColor,
+              child: Text('Add Task'),
+              onPressed: () {
+                if (formKey.currentState.validate()) {
+                  // here storing in firebase
+                  var user = FirebaseFirestore.instance.collection('tasks');
+                  user
+                      .add({
+                        "title": "Demo",
+                        "task": "A new demo task is added almost"
+                      })
+                      .then((result) => print('Store Success'))
+                      .catchError((error) => print(error));
+                  // *************
+                }
+                print('Title : ${taskTitle.value}');
+                print('Task : ${task.value}');
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('Processing Data')));
+              })
+        ]),
+      ),
+    );
+  }
+}
